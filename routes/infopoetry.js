8c2530666cb58bb02e9973f0c810e3d8f6a41d32
michelebruno <43304601+michelebruno@users.noreply.github.com>
2022-01-06 15:37:54 +0100

    Python test

--- routes/infopoetry.js
+++ routes/infopoetry.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const infopoetry = express.Router();
 
+let {PythonShell} = require('python-shell')
+
 // middleware that is specific to this router
-infopoetry.use(function timeLog (req, res, next) {
+infopoetry.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now())
     next()
 })
 // define the home page route
 infopoetry.get('/', function (req, res) {
-    res.json({
-        connected: 'ok'
+
+    PythonShell.run('python/infopoetry.py', {
+        args: ['ciao'],
+        pythonOptions: ['-u'], // get print results in real-time
+
+    }, function (err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        console.log('results: %j', results);
+
+        res.json(results)
     })
+
 })
 
 module.exports = infopoetry
